a0a66781417846d665775184e29f3a6676a04d2a
DeltaHinge <78765798+DeltaHinge@users.noreply.github.com>
2021-02-22 16:43:48 -0500

    Create LED_Bar_Graph.ino

---
+++ playing with code/LED_Bar_Graph_two _shift_registers/LED_Bar_Graph.ino
@@ -0,0 +1,22 @@
+int latchPin=8;
+int dataPin=11;
+int clockPin=12;
+
+// byte ledBar=0x03;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(latchPin,OUTPUT);
+pinMode(dataPin,OUTPUT);
+pinMode(clockPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(latchPin,LOW);
+shiftOut(dataPin,clockPin,LSBFIRST,0xff);
+shiftOut(dataPin,clockPin,LSBFIRST,0xff03>>8);
+delay(1000);
+digitalWrite(latchPin,HIGH);
+}
